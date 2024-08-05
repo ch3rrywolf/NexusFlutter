aa28484e6f9d04a934bae7bf11bcf9d78a67be23
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-08-05 17:13:57 +0100

    dart datatype

--- nexus_dart/lib/main.dart
+++ nexus_dart/lib/main.dart
@@ -1,9 +1,25 @@
+// hi dart
+/*
 void main() {
   //print('Hola Hola amigo, Dart!');
+}
+// dart variables
+void main() {
   //String name = "wolf";
   //String name2 = "wolfs";
   //int age = 28;
   //print(name);
   //print("$name2");
   //print("$age");
+}
+*/
+// dart datatype
+void main() {
+  String name = "wolf";
+  int age = 28;
+  double solde = 5400;
+  bool tunisia = true;
+
+  print(
+      "my name is $name | Old $age Years with balance is $solde DT | my country Tunisia $tunisia");
 }
