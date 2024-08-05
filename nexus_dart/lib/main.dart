5ce1a121850d15d7cc01420e70b1b06cc660ab35
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-08-05 17:32:50 +0100

    dart null safety

--- nexus_dart/lib/main.dart
+++ nexus_dart/lib/main.dart
@@ -12,14 +12,20 @@ void main() {
   //print("$name2");
   //print("$age");
 }
-*/
 // dart datatype
 void main() {
-  String name = "wolf";
-  int age = 28;
-  double solde = 5400;
-  bool tunisia = true;
-
-  print(
-      "my name is $name | Old $age Years with balance is $solde DT | my country Tunisia $tunisia");
+  //String name = "wolf";
+  //int age = 28;
+  //double solde = 5400;
+  //bool tunisia = true;
+  //print("my name is $name | Old $age Years with balance is $solde DT | my country Tunisia $tunisia");
 }
+// dart null safety
+void main() {
+  //String name;
+  //String? name;
+  //late String name;
+  //name = "wolf";
+  //print(name.length);
+}
+*/
