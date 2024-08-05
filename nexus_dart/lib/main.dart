852368cd35d9088e6cd84c17328663723ab5c0ad
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-08-05 17:00:45 +0100

    dart variable

--- nexus_dart/lib/main.dart
+++ nexus_dart/lib/main.dart
@@ -1,3 +1,9 @@
 void main() {
-  print('Hola Hola amigo, Dart!');
+  //print('Hola Hola amigo, Dart!');
+  //String name = "wolf";
+  //String name2 = "wolfs";
+  //int age = 28;
+  //print(name);
+  //print("$name2");
+  //print("$age");
 }
