025ac539d7783bac3cc64c8387c65c2fffed9a7a
unknown <ch3rrywolf>
2024-09-06 22:31:40 +0100

    flutter image

--- nexus_flutter/testflutter/lib/main.dart
+++ nexus_flutter/testflutter/lib/main.dart
@@ -11,34 +11,40 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             appBar: AppBar(title: const Text("TAZZUS.apk")),
             body: Container(
-              decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  border: Border.all(color: Colors.blue),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.blue,
-                        offset: Offset(10, 10),
-                        spreadRadius: 2,
-                        blurRadius: 10),
-                    BoxShadow(
-                        color: Colors.red,
-                        offset: Offset(10, -10),
-                        spreadRadius: 2,
-                        blurRadius: 10),
-                  ]),
-              width: 400,
-              height: 100,
-              // color: Colors.black,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(6),
-              padding: EdgeInsets.all(2),
-              child: const Text("TAZZ US !",
-                  style: TextStyle(
-                      fontSize: 62,
-                      color: Colors.green,
-                      fontWeight: FontWeight.w900,
-                      fontStyle: FontStyle.italic)),
+              // decoration: BoxDecoration(
+              //     color: Colors.black,
+              //     borderRadius: BorderRadius.all(Radius.circular(10)),
+              //     border: Border.all(color: Colors.blue),
+              //     boxShadow: [
+              //       BoxShadow(
+              //           color: Colors.blue,
+              //           offset: Offset(10, 10),
+              //           spreadRadius: 2,
+              //           blurRadius: 10),
+              //       BoxShadow(
+              //           color: Colors.red,
+              //           offset: Offset(10, -10),
+              //           spreadRadius: 2,
+              //           blurRadius: 10),
+              //     ]),
+              // width: 400,
+              // height: 100,
+              // // color: Colors.black,
+              // alignment: Alignment.center,
+              // margin: EdgeInsets.all(6),
+              // padding: EdgeInsets.all(2),
+              // child: const Text("TAZZ US !",
+              //     style: TextStyle(
+              //         fontSize: 62,
+              //         color: Colors.green,
+              //         fontWeight: FontWeight.w900,
+              //         fontStyle: FontStyle.italic)),
+              child: Image.asset(
+                fit: BoxFit.cover,
+                "images/taz.jpg",
+                width: 500,
+                height: 250,
+              ),
             )));
   }
 }
