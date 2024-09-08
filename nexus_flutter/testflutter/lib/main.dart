a4b5176067fadba45a1d8b3f0ade83973b6e51df
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-09 00:49:34 +0100

    flutter Example .

--- nexus_flutter/testflutter/lib/main.dart
+++ nexus_flutter/testflutter/lib/main.dart
@@ -23,9 +23,62 @@ class MyApp extends StatelessWidget {
                       fontStyle: FontStyle.italic))),
           // body: Container(child: Icon(Icons.star)),
           body: Container(
+              padding: EdgeInsets.all(10),
               child: Column(
-            children: [],
-          ))),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xffe6f0fa),
+                        border: Border.all(color: Colors.black, width: 2)),
+                    width: 400,
+                    padding: EdgeInsets.all(10),
+                    // color: Colors.amber,
+                    child: Text(
+                      "hi all",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.green),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    // margin: EdgeInsets.all(value),
+                    decoration: BoxDecoration(
+                        color: Color(0xffe6f0fa),
+                        border: Border.all(color: Colors.black, width: 2)),
+                    width: 400,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    // color: Colors.amber,
+                    child: Text(
+                      "hi alli ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali al",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.green),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Color(0xffe6f0fa),
+                        border: Border.all(color: Colors.black, width: 2)),
+                    width: 400,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                    // color: Colors.amber,
+                    child: Text(
+                      "hi alli ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali ali al",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.green),
+                    ),
+                  ),
+                ],
+              ))),
     );
   }
 }
