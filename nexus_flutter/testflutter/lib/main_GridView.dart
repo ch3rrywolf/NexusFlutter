8866c59cf6ba847f17ef7ac6113b6217d3effebf
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-08 23:09:38 +0100

    flutter GridView .

---
+++ nexus_flutter/testflutter/lib/main_GridView.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // final List employee = [
+  //   {"name": "test1", "age": "14"},
+  //   {"name": "test2", "age": "15"},
+  //   {"name": "test3", "age": "205"},
+  //   {"name": "test1", "age": "14"},
+  //   {"name": "test2", "age": "15"},
+  //   {"name": "test3", "age": "205"},
+  //   {"name": "test3", "age": "205"}
+  // ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+          title: const Text("TAZZUS.apk",
+              style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.green,
+                  fontWeight: FontWeight.w900,
+                  backgroundColor: Colors.white,
+                  fontStyle: FontStyle.italic))),
+      body: Container(
+          child: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5,
+        children: [
+          Container(child: Text("test"), color: Colors.black),
+          Container(child: Text("test"), color: Colors.white),
+          Container(child: Text("test"), color: Colors.red),
+          Container(child: Text("test"), color: Colors.green),
+          Container(child: Text("test"), color: Colors.yellow),
+          Container(child: Text("test"), color: Colors.grey),
+          Container(child: Text("test"), color: Colors.blue),
+        ],
+      )),
+    ));
+    // child: GridView.builder
+    // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //     crossAxisCount: 2,
+    //     mainAxisSpacing: 5.0,
+    //     crossAxisSpacing: 5,
+    //     mainAxisExtent: 150.0),
+  }
+}
