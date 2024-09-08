ad44c8359910834fa9b06dc617332d8b9b8c58d5
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-09 00:18:44 +0100

    flutter Expanded

---
+++ nexus_flutter/testflutter/lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              title: const Text("TAZZUS.apk",
+                  style: TextStyle(
+                      fontSize: 60,
+                      color: Colors.green,
+                      fontWeight: FontWeight.w900,
+                      backgroundColor: Colors.white,
+                      fontStyle: FontStyle.italic))),
+          // body: Container(child: Icon(Icons.star)),
+          body: Container(
+              child: Row(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.red,
+                    height: 100,
+                  )),
+              Expanded(
+                  child: Container(
+                color: Colors.blue,
+                height: 100,
+              )),
+              Expanded(
+                  child: Container(
+                color: Colors.yellow,
+                height: 100,
+              )),
+            ],
+          ))),
+    );
+  }
+}
