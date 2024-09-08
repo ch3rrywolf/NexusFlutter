d8c9a4ea357e7d5395d39280b311a3051cd8b14c
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-09 00:27:36 +0100

    flutter Example

---
+++ nexus_flutter/testflutter/lib/main.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              title: const Text("TAZZUS.apk",
+                  style: TextStyle(
+                      fontSize: 60,
+                      color: Colors.green,
+                      fontWeight: FontWeight.w900,
+                      backgroundColor: Colors.white,
+                      fontStyle: FontStyle.italic))),
+          // body: Container(child: Icon(Icons.star)),
+          body: Container(
+              child: Column(
+            children: [],
+          ))),
+    );
+  }
+}
