d42d416ef26186c5220770827d2ad3da9eaf6fc8
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-10 23:11:57 +0100

    flutter Stack

---
+++ nexus_flutter/testflutter/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool statusA = false;
+  bool statusB = false;
+  bool statusC = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text("TAZZUS.apk",
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.green,
+                    fontWeight: FontWeight.w900,
+                    backgroundColor: Colors.white,
+                    fontStyle: FontStyle.italic))),
+        body: Container(
+            padding: EdgeInsets.all(10),
+            child: Stack(
+              children: [
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.amber,
+                ),
+                Positioned(
+                    bottom: 0,
+                    child:
+                        Container(width: 200, height: 200, color: Colors.red)),
+              ],
+            )),
+      ),
+    );
+  }
+}
