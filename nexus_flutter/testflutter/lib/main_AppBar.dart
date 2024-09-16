5c0fbf6dd50420714f341f9814acfc955fadc122
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-16 23:04:06 +0100

    flutter AppBar

---
+++ nexus_flutter/testflutter/lib/main_AppBar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  GlobalKey<FormState> formstate = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("UnderGround's"),
+            centerTitle: true,
+            backgroundColor: Colors.green[400],
+            leading: Text("Go!"),
+            elevation: 20.0,
+            shadowColor: Colors.red,
+            titleTextStyle:
+                TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.start))],
+          ),
+          body: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [],
+            ),
+          )),
+    );
+  }
+}
