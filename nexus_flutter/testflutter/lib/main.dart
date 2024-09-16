bcf1c1da2c56b381175b82b6d8c5bddb81bf66f2
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-16 22:42:41 +0100

    flutter TextFormField onSave

--- nexus_flutter/testflutter/lib/main.dart
+++ nexus_flutter/testflutter/lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   GlobalKey<FormState> formstate = GlobalKey();
+  String? username;
+  String? phone;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,6 +34,10 @@ class _MyAppState extends State<MyApp> {
               child: Column(
                 children: [
                   TextFormField(
+                    onSaved: (val) {
+                      username = val;
+                    },
+                    decoration: InputDecoration(hintText: "username"),
                     validator: (value) {
                       if (value!.isEmpty) {
                         return "champ vide !";
@@ -44,9 +50,11 @@ class _MyAppState extends State<MyApp> {
                   MaterialButton(
                     onPressed: () {
                       if (formstate.currentState!.validate()) {
-                        print("valid");
-                      } else {
+                        formstate.currentState!.save();
                         print("not vide");
+                        print(username);
+                      } else {
+                        print("vide");
                       }
                     },
                     child: Text("Valid"),
@@ -58,3 +66,80 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   GlobalKey<FormState> formstate = GlobalKey();
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//             title: const Text("TAZZUS.apk",
+//                 style: TextStyle(
+//                     fontSize: 60,
+//                     color: Colors.green,
+//                     fontWeight: FontWeight.w900,
+//                     backgroundColor: Colors.white,
+//                     fontStyle: FontStyle.italic))),
+//         body: Container(
+//             padding: EdgeInsets.all(10),
+//             child: Form(
+//               key: formstate,
+//               child: Column(
+//                 children: [
+//                   TextFormField(
+//                     validator: (value) {
+//                       if (value!.isEmpty) {
+//                         return "champ vide !";
+//                       }
+//                       if (value.length < 5) {
+//                         return "under 5 not valide";
+//                       }
+//                     },
+//                   ),
+//                   MaterialButton(
+//                     onPressed: () {
+//                       if (formstate.currentState!.validate()) {
+//                         print("valid");
+//                       } else {
+//                         print("not vide");
+//                       }
+//                     },
+//                     child: Text("Valid"),
+//                   )
+//                 ],
+//               ),
+//             )),
+//       ),
+//     );
+//   }
+// }
+
+
+
