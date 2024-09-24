3110fb369cd2a334903d8f398cb5455055b568ec
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-09-24 16:28:16 +0100

    flutter Tabbar fin.

--- nexus_flutter/testflutter/lib/main.dart
+++ nexus_flutter/testflutter/lib/main.dart
@@ -1,3 +1,69 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: DefaultTabController(
+//       length: 3,
+//       initialIndex: 0,
+//       child: Scaffold(
+//           key: scaffoldkey,
+//           appBar: AppBar(
+//             title: Text("GoGo!!"),
+//             bottom: TabBar(
+//                 indicatorColor: Colors.green,
+//                 indicatorWeight: 4,
+//                 labelColor: Colors.green,
+//                 labelStyle: TextStyle(fontSize: 20),
+//                 unselectedLabelColor: Colors.grey,
+//                 unselectedLabelStyle: TextStyle(fontSize: 10),
+//                 tabs: [
+//                   Tab(
+//                     iconMargin: EdgeInsets.all(5),
+//                     icon: Icon(Icons.start),
+//                     text: "Start",
+//                     // child: Text("Start"),
+//                   ),
+//                   Tab(
+//                     icon: Icon(Icons.payment),
+//                     text: "Pay",
+//                     // child: Text("Pay"),
+//                   ),
+//                   Tab(
+//                     icon: Icon(Icons.error),
+//                     text: "Die",
+//                     // child: Text("Die"),
+//                   )
+//                 ]),
+//           ),
+//           body: Container(
+//             padding: EdgeInsets.all(10),
+//             child: TabBarView(
+//               children: [
+//                 Text("Welcome bro"),
+//                 Text("last coin"),
+//                 Text("mais dabor tché tché"),
+//               ],
+//             ),
+//           )),
+//     ));
+//   }
+// }
+//*********************************************************************** */
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,20 +77,23 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  TabController? tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: DefaultTabController(
-      length: 3,
-      initialIndex: 0,
-      child: Scaffold(
-          key: scaffoldkey,
+      home: Scaffold(
           appBar: AppBar(
             title: Text("GoGo!!"),
             bottom: TabBar(
+                controller: tabController,
                 indicatorColor: Colors.green,
                 indicatorWeight: 4,
                 labelColor: Colors.green,
@@ -53,13 +122,36 @@ class _MyAppState extends State<MyApp> {
           body: Container(
             padding: EdgeInsets.all(10),
             child: TabBarView(
+              controller: tabController,
               children: [
-                Text("Welcome bro"),
-                Text("last coin"),
+                Column(
+                  children: [
+                    Text("Welcome bro"),
+                    MaterialButton(
+                      onPressed: () {
+                        tabController!.animateTo(1);
+                      },
+                      child: Text("Click To Go"),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("last coin"),
+                    MaterialButton(
+                      onPressed: () {
+                        tabController!.animateTo(2);
+                      },
+                      child: Text("Click To Die"),
+                    )
+                  ],
+                ),
+                // Text("Welcome bro"),
+
                 Text("mais dabor tché tché"),
               ],
             ),
           )),
-    ));
+    );
   }
 }
